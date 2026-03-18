bea9e931382f665d6caaa5f21eb1e7127c4d3aff
Shivam Kishore <shivamkishore009@gmail.com>
2026-03-19 03:39:41 +0530

    added url of deployed fend and bend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:8000"
+      "/api": "https://pingster-tone.onrender.com"
     }
   }
 })
