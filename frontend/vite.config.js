65653e154904cee4505783bb09f04dec50b89c98
Shivam Kishore <shivamkishore009@gmail.com>
2026-03-19 04:05:08 +0530

    SOCKET IO ENDPOINT FIXED d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      "/api": "https://pingster-tone.onrender.com"
-    }
-  }
+  plugins: [react(), tailwindcss()],
 })
